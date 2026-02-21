a6f77d237ddd82579292170085269cee7573f895
Ivan Karpukhin <karpuhini@yandex.ru>
2026-02-21 18:09:31 +0300

    Fix grad normalizer device

--- aligned_hpo/gradient.py
+++ aligned_hpo/gradient.py
@@ -17,30 +17,25 @@ class GradientNormalizer(torch.nn.Module):
         self.register_buffer("moving_norm", torch.zeros([]))
 
     def forward(self, parameters):
-        parameters = list(parameters)
+        parameters = [p for p in parameters if p.requires_grad and (p.grad is not None)]
+        if not parameters:
+            return 0
+        device = parameters[0].device
+        if self.moving_norm.device != device:
+            self.to(device)
         with torch.no_grad():
             norm = self._compute_grad_norm(parameters)
-            if norm is None:
-                return norm
             momentum = 0 if self.is_first else self._momentum
             self.moving_norm.fill_(self.moving_norm * momentum + (1 - momentum) * norm)
             mean = self.moving_norm.clip(min=self._clip).item()
-            self._mul_grad(parameters, 1 / mean)
+            for p in parameters:
+                p.grad /= mean
             self.is_first.fill_(False)
         return norm / mean
 
     @staticmethod
     def _compute_grad_norm(parameters):
-        parameters = [p for p in parameters if p.grad is not None]
-        if not parameters:
-            return None
-        device = parameters[0].grad.device
-        norm = torch.norm(torch.stack([torch.norm(p.grad.detach()).to(device) for p in parameters]))
-        return norm
-
-    @staticmethod
-    def _mul_grad(parameters, alpha):
-        for p in parameters:
-            if (not p.requires_grad) or (p.grad is None):
-                continue
-            p.grad *= alpha
+        norms = torch.zeros(len(parameters), device=parameters[0].device)
+        for i, p in enumerate(parameters):
+            norms[i] = p.grad.data.norm(2)
+        return norms.square().sum().item() ** 0.5
